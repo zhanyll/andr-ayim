acc520e4ebfc363d013cb7e945b8b9fc240e54bb
zhanyll <janylayka95@gmail.com>
2021-12-01 12:03:47 +0600

    1.11.2021

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.andrayim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<AppCompatButton>(R.id.btn)
+        btn.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/andrayim/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.andrayim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, Fragment1())
+            .commit()
+    }
+}
