19aeee29d66ca511c6498522479195c56841428b
zhanyll <janylayka95@gmail.com>
2021-12-06 17:09:48 +0600

    hw10

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.andrayim
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txt = findViewById<AppCompatTextView>(R.id.result)
+        val expr = "2+2*2"
+        val exp = ExpressionBuilder(expr).build()
+        val res = exp.evaluate()
+        txt.text = res.toString()
     }
 }
