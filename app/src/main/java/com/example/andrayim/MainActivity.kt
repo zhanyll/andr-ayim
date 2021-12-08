bf0ad11e3efea5c4fa966a3c1ad69a4a29aea911
zhanyll <janylayka95@gmail.com>
2021-12-08 17:45:14 +0600

    lesson29

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.andrayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.andrayim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            val prefs = getSharedPreferences("MyAppPrefs", MODE_PRIVATE)
+
+            txt.text = prefs.getString("key", "defValue")
+
+            btn.setOnClickListener {
+                val editor = prefs.edit()
+                editor.putString("key", edit.text.toString())
+                editor.apply()
+            }
+        }
     }
 }
