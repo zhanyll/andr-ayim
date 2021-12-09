a9be5d830af3cafdaa7333b4889e5ccdc91d5eb4
zhanyll <janylayka95@gmail.com>
2021-12-10 01:34:11 +0600

    hw11 with email

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.andrayim
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.OutcomeReceiver
+import android.widget.Toast
+import com.example.andrayim.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val receiver = binding.receiver.text.toString()
+        val subject = binding.subject.text.toString()
+        val text = binding.text.text.toString()
+
+        binding.btn.setOnClickListener { onClick(receiver, subject, text) }
+    }
+
+    fun onClick(receiver: String, subject: String, text: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.data = Uri.parse("mailto:")
+        intent.type = "text/plain"
+
+        intent.putExtra(Intent.EXTRA_EMAIL, receiver)
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, "$e", Toast.LENGTH_SHORT).show()
+        }
     }
 }
