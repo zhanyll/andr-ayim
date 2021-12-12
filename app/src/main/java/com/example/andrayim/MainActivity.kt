bda1bc8d96635e523e1193dbb4d25f15be955451
zhanyll <janylayka95@gmail.com>
2021-12-12 20:48:28 +0600

    homework 13 orientation

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.andrayim
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> setContentView(R.layout.activity_main)
+            Configuration.ORIENTATION_LANDSCAPE -> setContentView(R.layout.activity_main_horizontal)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_1, Fragment1())
+            .addToBackStack(null)
+            .commit()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_2, Fragment2())
+            .addToBackStack(null)
+            .commit()
     }
 }
