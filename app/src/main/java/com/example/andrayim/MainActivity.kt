def94fab94e77499257cf44eacaa21827a066185
Kudaybergen <tuleubekov.kas@gmail.com>
2021-12-20 10:34:55 +0600

    comment: hw-11. Почему не подставляются эдиты

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -17,14 +17,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val receiver = binding.receiver.text.toString()
-        val subject = binding.subject.text.toString()
-        val text = binding.text.text.toString()
+        val receiver = binding.receiver.text.toString() // Вы берете значения из эдитТекстов в методе onCreate(),
+        val subject = binding.subject.text.toString() // -> когда приложение ток запустилось
+        val text = binding.text.text.toString() // -> на старте эдитТексты пустые и сохраняете пустые строки в переменные
 
         binding.btn.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             val receivers = receiver.split(",".toRegex()).toTypedArray()
-            intent.putExtra(Intent.EXTRA_EMAIL, receivers)
+            intent.putExtra(Intent.EXTRA_EMAIL, receivers) // и здесь передаете пустые строки -> ""
             intent.putExtra(Intent.EXTRA_SUBJECT, subject)
             intent.putExtra(Intent.EXTRA_TEXT, text)
             intent.type = "message/rfc822"
