ce9cc9c5b1dc6b89a6131846eb9eb52df36c510b
zhanyll <janylayka95@gmail.com>
2021-12-27 18:44:49 +0600

    homework34 regex

--- app/src/main/java/com/example/andrayim/MainActivity.kt
+++ app/src/main/java/com/example/andrayim/MainActivity.kt
@@ -13,9 +13,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btn.setOnClickListener {
-            val text = binding.editText.text.toString()
-            val result = Regex("""(\s+|(\r\n|\r|\n))""").findAll(text.trim()).count() + 1
+            val text = binding.editText.text.toString().trim()
+//            val oo = "o".toRegex()
+//            val result = oo.findAll(text).count()
+            val result = findO(text)
             binding.txt.text = result.toString()
         }
+    }
+
+    private fun findO(text: String): Int {
+        val lst = text.split(" ")
+        var res = 0
+        val o = "o".toRegex()
+        for (i in lst) {
+            val count = o.findAll(i).count()
+            if (i.contains("o") && count == 1) {
+                res += 1
+            }
+        }
+        return res
     }
 }
